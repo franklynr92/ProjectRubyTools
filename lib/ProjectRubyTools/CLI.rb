a4492cbe2d400427a9293f65f6dd2e42f6bd7d3f
Franklyn Rodriguez <franklynr92@gmail.com>
2020-05-09 03:39:45 -0400

    CLI has more exit features, and some dialogue, added more features, still needs second Scraper method for second site....ALMOST THEREg

--- lib/ProjectRubyTools/CLI.rb
+++ lib/ProjectRubyTools/CLI.rb
@@ -14,13 +14,15 @@ class ProjectRubyTools::CLI
         puts "Hello there!"
         sleep(1)
         puts "This is a one stop shop for Ruby projects or a better used term...gems!"
-        sleep(3)
-        puts "Please press enter to continue or.."
-        puts "...type exit to leave"
+        sleep(2)
+        puts "Please press enter to continue .."
+        puts "...type exit to leave or exit! for a quick haste"
         input = gets.chomp
         if input == "exit"
         puts "Awh okay"
-           goodbye
+           quickExit
+        elsif input =="exit!"
+            goodbye
         end
     end  
 
@@ -29,15 +31,27 @@ class ProjectRubyTools::CLI
         puts "What is your name friend?"
         name = gets.chomp
         self.username = name
+        if name == "exit"
+            quickExit
+        elsif name == ""
+            self.username = "Captain"
         puts "Welcome, #{self.username}"
         sleep(2)
         scrape1
     end
+end
 
 
     def scrape1
+        puts "#{self.username}, if you could please wait a moment"
+        sleep(0.5)
+        puts "We'll be right with you shortly"
+        sleep(0.3)
+        puts "Thank you!"
         sleep(1)
         ProjectRubyTools::Scraper.scrape_ruby
+        puts "Get ready to... Yo-ho-ho!"
+        sleep(1)
         system("clear")
         sleep(1)
         display
@@ -72,8 +86,12 @@ class ProjectRubyTools::CLI
     def display
         puts "#{self.username}"
         puts "These are the categories: "
-        ProjectRubyTools::GemCategory.all.each.with_index(1) {|element, i| puts "#{i}. Category #{element.category} : #{element.category_description}, 
-        it's most popular projects #{element.most_popular_projects}"}
+        ProjectRubyTools::GemCategory.all.each.with_index(1) {|element, i| puts "For #{i}. the category is... #{element.category} 
+        a brief description of  what #{element.category} does is the following:
+        #{element.category_description}, 
+
+        it's two most popular projects are: 
+        #{element.most_popular_projects}"}
         menu
     end
 
@@ -96,14 +114,18 @@ class ProjectRubyTools::CLI
         
         puts "These are the categories:"
         # binding.pry
-        ProjectRubyTools::GemCategory.all.each { |element| puts "#{element.category}"}# - #{category.price} - #{category.availability}"}
+        ProjectRubyTools::GemCategory.all.each.with_index(1) { |element,i| puts "#{i}. #{element.category}"}# - #{category.price} - #{category.availability}"}
       #   @categories.each.with_index(1) do |category, i|
       #          puts "#{i}. #{category.name}"
       # end
     end
 
     def category_description
-        ProjectRubyTools::GemCategory.all.each { |element| puts "#{element.category_description}"}
+        puts "These are all the categories with the coninsiding descriptions"
+        sleep(1)
+        ProjectRubyTools::GemCategory.all.each.with_index(1) {|element, i| puts "#{i}. #{element.category} : 
+         #{element.category}"}
+         menu
     end
 
     def most_popular_projects
@@ -142,8 +164,8 @@ class ProjectRubyTools::CLI
             elsif input == "list"
                 list_category
                 self.menu
-            elsif input == "category description"
-               list_category + category_description
+            elsif input == "category description" || input == "cat description"
+               category_description
                 self.menu
             elsif input == "most popular projects"
                 # binding.pry
@@ -170,12 +192,61 @@ class ProjectRubyTools::CLI
     end
     end
 
+    def quickExit
+        puts "So soon?!"
+        puts "Press enter to contiue...exit to leave, Cap'n for a Captain's farewell, goodbye for a farewell song"
+        input = gets.chomp
+        if input == "exit"
+            puts "at last matey"
+            puts "We must part ways"
+            sleep(1.5)
+            abort("Life's uncharted Journerys")
+        elsif input == "Cap'n"
+            goodbye
+        elsif input == "goodbye"
+            puts "so long.. " 
+            sleep(1.2) 
+            puts "farewell" 
+            sleep(2.0)
+            puts "to you" 
+            sleep(2.0)
+            puts "my friends" 
+            sleep(4.0)
+            puts "Goodbye" 
+            sleep(2.0) 
+            puts "for now," 
+            puts "until we meet again"
+            sleep(2.0)
+               abort("It's been fun!")
+        else
+            get_user_name
+        end
+    end
+
+=begin    def first_note(a,b,c,d,e,f,g)
+        a = "so long..."
+        b = "farewell"
+        c = "to you"
+        d = "my friends"
+        e = "Goodbye"
+        f = "for now"
+        g = "until we meet again"
+    end
+=end
+    def second_note
+        puts "farewell"
+    end
+
     def goodbye
+        system("clear")
         puts "Goodbye, see you later aligator!!"
-        puts "at last matey"
         puts "She cannot take any more of this, Captain!" 
         sleep(2)
-        abort("Hello world!")
+        puts"Goodbye..
+        until we sea shall each other again!
+            Fair Winds and Following Seas."
+        sleep(3)
+        abort()
     end
   
 
