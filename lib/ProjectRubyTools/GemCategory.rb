6c40d33522d136612b657043af7d1659916d8707
Franklyn Rodriguez <franklynr92@gmail.com>
2020-09-13 19:52:29 -0400

    moving comments to the bottom to better read code

--- lib/ProjectRubyTools/GemCategory.rb
+++ lib/ProjectRubyTools/GemCategory.rb
@@ -1,22 +1,17 @@
-#this class handles all the gems
+
 class ProjectRubyTools::GemCategory
   
-attr_accessor :category, :category_description, :most_popular_projects, :other_projects_link, :project_name, :project_description #, :project_tagline, :project_description, :project_downloads
+attr_accessor :category, :category_description, :most_popular_projects, :other_projects_link, :project_name, :project_description 
 
 @@all = []
 
-    def initialize(category, category_description, most_popular_projects, other_projects_link, project_name = nil, project_description = nil) #project_tagline, project_description, project_downloads) # projects)
+    def initialize(category, category_description, most_popular_projects, other_projects_link, project_name = nil, project_description = nil) 
       @category = category
       @category_description = category_description
       @most_popular_projects = most_popular_projects
       @other_projects_link = other_projects_link
      @project_name = project_name
      @project_description = project_description
-=begin      @project_tagline = project_tagline
-      @project_description = project_description
-      @project_downloads = project_downloads
-      @projects = projects
-=end      #binding.pry
       @@all << self
     end
   
@@ -24,9 +19,18 @@ attr_accessor :category, :category_description, :most_popular_projects, :other_p
       @@all
     end
 
-   #def self.find_or_create_by_name(category)
-   # self.all.find{|project| project == project}
-   # end
 
     
 end
+#this class handles all the gems
+#, :project_tagline, :project_description, :project_downloads
+#project_tagline, project_description, project_downloads) # projects)
+=begin      @project_tagline = project_tagline
+      @project_description = project_description
+      @project_downloads = project_downloads
+      @projects = projects
+=end      #binding.pry
+
+#def self.find_or_create_by_name(category)
+   # self.all.find{|project| project == project}
+   # end
